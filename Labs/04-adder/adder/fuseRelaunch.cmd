270c0d2bcfcf3648e85f67d06ecf61b0e4544a6c
Guillaume Dussol <guillaume.dussol@gmail.com>
2020-02-27 10:04:06 +0100

    adding lab 04

---
+++ Labs/04-adder/adder/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/lab661/Documents/xmarek65/Digital-electronics-1/Labs/04-adder/adder/test2_isim_beh.exe" -prj "/home/lab661/Documents/xmarek65/Digital-electronics-1/Labs/04-adder/adder/test2_beh.prj" "work.test2" 
